289993c35b8ae39f25e5b3d54a583c41db381c08
jesterx7 <marcellinoagusitnus@ymail.com>
2019-04-22 11:18:12 +0700

    Updating drawer for general user

--- app/src/main/java/com/example/user/wearemachungers/MainActivity.java
+++ app/src/main/java/com/example/user/wearemachungers/MainActivity.java
@@ -80,17 +80,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_berita) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_newsletter) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_agenda) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_profil) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_faq) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_life_at) {
 
         }
 
